bf8de36348bf90390e24ef7e5a79e4c5f120c602
Nate Christensen <natec8@gmail.com>
2016-11-07 10:39:42 -0700

    Initial Commit, Begin Adding Development Files

---
+++ README.md
@@ -0,0 +1,3 @@
+# DDM React CropperJS
+
+# TODO add example of using the component and document the props
---
+++ modules/ReactCropperJS.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Cropper from 'cropperjs';
+
+class ReactCropperJS extends Component
+{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default ReactCropperJS;
