5cd916cbeaf83a92301f38ff82fc0d104cd83922
Roman Romanchukevych <rromanch@softserveinc.com>
2016-11-08 17:55:58 +0200

    Adds a npm `server` command to run webpack server + Fixes showing crop preview + Adds a block with crop details + Adds styles for preview and details blocks + Updates `aspectRatio` to match 406/195

--- example/example.jsx
+++ example/example.jsx
@@ -1,29 +1,36 @@
-import ReactCropper from './react-cropper';
 import React from 'react';
 import ReactDom from 'react-dom';
+import ReactCropper from './react-cropper';
 import {} from './styles.scss';
 
 const Wrapper = (props) => {
   return (
     <div className="cropperjs-wrapper">
-      <ReactCropper {...props} />
       <div id="crop-preview"></div>
+      <div className="crop-details"></div>
+      <ReactCropper {...props} />
     </div>
   );
 };
 
 const crop = (data) => {
   console.log(data);
+  let details = document.querySelector('.crop-details');
+  details.innerHTML = JSON.stringify(data.detail);
 }
 
 let props = {
   src: '../images/demo.jpg',
   alt: 'Demo Image',
   crossOrigin: 'false',
-  aspectRatio: (16 / 9),
+  aspectRatio: (406 / 195),
   guides: true,
   crop: crop,
-  preview: '#crop-preview'
+  preview: '#crop-preview',
+  viewMode: 1,
+  // Disable toggling drag mode between "crop" and "move" on doubleclick
+  toggleDragModeOnDblclick: false,
+  dragMode: 'move'
 }
 
 ReactDom.render(
